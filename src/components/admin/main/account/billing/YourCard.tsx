8facfe565882455025f3846094ad773ff5d356f1
unknown <dsselkari@gmail.com>
2024-03-14 12:28:22 +0530

    card removed-2

--- src/components/admin/main/account/billing/YourCard.tsx
+++ src/components/admin/main/account/billing/YourCard.tsx
@@ -1,5 +1,5 @@
-import Card from 'components/card';
-import MasterCard from 'components/card/MasterCard';
+// import Card from 'components/card';
+// import MasterCard from 'components/card/MasterCard';
 import { AiOutlinePlus } from 'react-icons/ai';
 //assets
 import { MdCached, MdAdd, MdAttachMoney, MdMoreHoriz } from 'react-icons/md';
