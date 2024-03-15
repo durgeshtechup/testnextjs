23cc41ae38ff59153921881b2676274c7f1e141c
unknown <dsselkari@gmail.com>
2024-03-15 10:32:15 +0530

    hemburger menu added

--- src/components/navbar/index.tsx
+++ src/components/navbar/index.tsx
@@ -20,6 +20,7 @@ import { getProfile } from 'api/users';
 import { getSingleRole } from 'api/role';
 import toast from 'react-hot-toast';
 import { ClientContext } from 'clientProvider';
+import { RiMoonFill, RiSunFill } from 'react-icons/ri';
 
 
 const Navbar = (props: {
@@ -74,6 +75,7 @@ const Navbar = (props: {
           : 'w-[calc(100vw_-_6%)] md:w-[calc(100vw_-_8%)] lg:w-[calc(100vw_-_6%)] xl:w-[calc(100vw_-_180px)] 2xl:w-[calc(100vw_-_195px)]'
       }  p-2 backdrop-blur-xl dark:bg-[#0b14374d] md:right-[30px] md:top-4 xl:top-[20px]`}
     >
+
       <div className="ml-[6px]">
        {false&& <div className="h-6 w-[224px] pt-1">
           <a
@@ -104,7 +106,9 @@ const Navbar = (props: {
       </div>
 
       {/* <div className="relative mt-[3px] flex h-[61px] w-[355px] flex-grow items-center justify-around gap-2 rounded-full bg-white px-2 py-2 shadow-xl shadow-shadow-500 dark:!bg-navy-800 dark:shadow-none md:w-[365px] md:flex-grow-0 md:gap-1 xl:w-[365px] xl:gap-2"> */}
-      <div className="relative mt-[3px] flex h-[61px]  flex-grow items-center justify-around gap-2 rounded-full bg-white px-2 py-2 shadow-xl shadow-shadow-500 dark:!bg-navy-800 dark:shadow-none  md:flex-grow-0 md:gap-1  xl:gap-2">
+      {/* <div className="relative mt-[3px] flex h-[61px]  flex-grow items-center justify-around gap-2 rounded-full bg-white px-2 py-2 shadow-xl shadow-shadow-500 dark:!bg-navy-800 dark:shadow-none  md:flex-grow-0 md:gap-1  xl:gap-2"> */}
+      <div className="relative flex place-content-start items-center justify-around gap-2 rounded-full bg-white px-2 py-2 shadow-xl shadow-shadow-500 dark:!bg-navy-800 dark:shadow-none md:w-fit md:flex-grow-0 md:gap-1 xl:w-fit xl:gap-3" style={{ margin: "0px 10px" }}>
+     
         {/* <div className="flex h-full items-center rounded-full bg-lightPrimary text-navy-700 dark:bg-navy-900 dark:text-white xl:w-[225px]">
           <p className="pl-3 pr-2 text-xl">
             <FiSearch className="h-4 w-4 text-gray-400 dark:text-white" />
@@ -115,12 +119,12 @@ const Navbar = (props: {
             className="block h-full w-full rounded-full bg-lightPrimary text-sm font-medium text-navy-700 outline-none placeholder:!text-gray-400 dark:bg-navy-900 dark:text-white dark:placeholder:!text-white sm:w-fit"
           />
         </div> */}
-        {/* <span
+        <span
           className="flex cursor-pointer text-xl text-gray-600 dark:text-white xl:hidden "
           onClick={onOpenSidenav}
         >
           <FiAlignJustify className="h-5 w-5" />
-        </span> */}
+        </span>
         {/* start Notification */}
         {/* <Dropdown
           button={
@@ -220,7 +224,7 @@ const Navbar = (props: {
           darkmode={darkmode}
           setDarkmode={setDarkmode}
         /> */}
-        <div
+        {/* <div
           className="cursor-pointer text-gray-600"
           onClick={() => {
             if (darkmode) {
@@ -231,7 +235,26 @@ const Navbar = (props: {
               setDarkmode(true);
             }
           }}
-        ></div>
+        ></div> */}
+
+<div
+          className="cursor-pointer pl-1 text-gray-600"
+          onClick={() => {
+            if (darkmode) {
+              document.body.classList.remove("dark");
+              setDarkmode(false);
+            } else {
+              document.body.classList.add("dark");
+              setDarkmode(true);
+            }
+          }}
+        >
+          {darkmode ? (
+            <RiSunFill className="h-5 w-5 text-gray-600 dark:text-white" />
+          ) : (
+            <RiMoonFill className="h-5 w-5 text-gray-600 dark:text-white" />
+          )}
+        </div>
         {/* Profile & Dropdown */}
         <Dropdown
           button={
