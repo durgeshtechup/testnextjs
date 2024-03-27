96a9aca7efa18cf615c68ace3728b2f78e4cafb7
unknown <dsselkari@gmail.com>
2024-03-27 14:18:27 +0530

    tripple toggle added

--- src/components/navbar/index.tsx
+++ src/components/navbar/index.tsx
@@ -2,10 +2,10 @@ import React, { useContext, useEffect, useState } from 'react';
 import Dropdown from 'components/dropdown';
 import { FiAlignJustify } from 'react-icons/fi';
 import NavLink from 'components/link/NavLink';
-import navbarimage from '/public/img/layout/Navbar.png';
-import { BsArrowBarUp } from 'react-icons/bs';
-import { FiSearch } from 'react-icons/fi';
-import Configurator from './Configurator';
+// import navbarimage from '/public/img/layout/Navbar.png';
+// import { BsArrowBarUp } from 'react-icons/bs';
+// import { FiSearch } from 'react-icons/fi';
+// import Configurator from './Configurator';
 // import { RiMoonFill, RiSunFill } from 'react-icons/ri';
 // import Configurator from './Configurator';
 import {
@@ -22,6 +22,7 @@ import toast from 'react-hot-toast';
 import { ClientContext } from 'clientProvider';
 import { RiMoonFill, RiSunFill } from 'react-icons/ri';
 import Link from 'next/link';
+import TripleToggleSwitch from 'app/admin/payout/components/TripleToggleSwitch';
 
 
 const Navbar = (props: {
@@ -32,7 +33,7 @@ const Navbar = (props: {
 }) => {
   const router=useRouter()
   const [storeName,setStoreName]=useState<any>({})
-  const {setSingleRoleData}=useContext(ClientContext)
+  const {setSingleRoleData, setFilter, filter,}=useContext(ClientContext)
 
   useEffect(() => {
     getProfile()
@@ -66,36 +67,43 @@ const Navbar = (props: {
     clearAllCookie();
     router.push("/auth/sign-in");
   };
+  const labels = {
+    left: {
+      title: "All",
+      value: "left",
+    },
+    right: {
+      title: "Live",
+      value: "right",
+    },
+    center: {
+      title: "Test",
+      value: "center",
+    },
+  };
+
+  const onChange = (value: any) => {
+    if (value === "left") {
+      setFilter({ ...filter, status: "all" });
+    } else if (value === "right") {
+      setFilter({ ...filter, status: "live" });
+    } else if (value === "center") {
+      setFilter({ ...filter, status: "test" });
+    }
+  };
   return (
     <nav
       className={`duration-175 linear shadow fixed right-3 top-3 flex flex-row flex-wrap items-center justify-between rounded-xl bg-white/30 transition-all ${
         mini === false
           ? 'w-[calc(100vw_-_6%)] md:w-[calc(100vw_-_8%)] lg:w-[calc(100vw_-_6%)] xl:w-[calc(100vw_-_350px)] 2xl:w-[calc(100vw_-_365px)]'
           : mini === true && hovered === true
-          ? 'w-[calc(100vw_-_6%)] md:w-[calc(100vw_-_8%)] lg:w-[calc(100vw_-_6%)] xl:w-[calc(100vw_-_350px)] 2xl:w-[calc(100vw_-_365px)]'
+          ? 'w-[calc(100vw_-_6%)]  md:w-[calc(100vw_-_8%)] lg:w-[calc(100vw_-_6%)] xl:w-[calc(100vw_-_350px)] 2xl:w-[calc(100vw_-_365px)]'
           : 'w-[calc(100vw_-_6%)] md:w-[calc(100vw_-_8%)] lg:w-[calc(100vw_-_6%)] xl:w-[calc(100vw_-_180px)] 2xl:w-[calc(100vw_-_195px)]'
-      }  p-2 backdrop-blur-xl dark:bg-[#0b14374d] md:right-[30px] md:top-4 xl:top-[20px] `}
+      }  p-2 backdrop-blur-xl dark:bg-[#0b14374d] md:right-[30px] md:top-4 xl:top-[20px]`}
     >
 
-      <div className="ml-[6px]">
-       {false&& <div className="h-6 w-[224px] pt-1">
-          <a
-            className="text-sm font-normal text-navy-700 hover:underline dark:text-white dark:hover:text-white"
-            href=" "
-          >
-            
-            <span className="mx-1 text-sm text-navy-700 hover:text-navy-700 dark:text-white">
-              {/* {' '} */}
-              {/* /{' '} */}
-            </span>
-          </a>
-          {/* <NavLink
-            className="text-sm font-normal capitalize text-navy-700 hover:underline dark:text-white dark:hover:text-white"
-            href="#"
-          >
-            {brandText}
-          </NavLink> */}
-        </div>}
+      <div className="ml-[6px] md:flex md:justify-between">
+     
         <p className="shrink text-[33px] capitalize text-navy-700 dark:text-white">
           <NavLink
             href="#"
@@ -104,7 +112,19 @@ const Navbar = (props: {
             {brandText}
           </NavLink>
         </p>
+
+        {brandText === "Transactions" && (
+            <div className="mb-1 flex ml-4">
+              <label className="relative mb-3 inline-flex cursor-pointer items-center">
+                <TripleToggleSwitch labels={labels} onChange={onChange} />
+              </label>
+            </div>
+          )}
+       
       </div>
+
+    
+     
 
       {/* <div className="relative mt-[3px] flex h-[61px] w-[355px] flex-grow items-center justify-around gap-2 rounded-full bg-white px-2 py-2 shadow-xl shadow-shadow-500 dark:!bg-navy-800 dark:shadow-none md:w-[365px] md:flex-grow-0 md:gap-1 xl:w-[365px] xl:gap-2"> */}
       {/* <div className="relative mt-[3px] flex h-[61px]  flex-grow items-center justify-around gap-2 rounded-full bg-white px-2 py-2 shadow-xl shadow-shadow-500 dark:!bg-navy-800 dark:shadow-none  md:flex-grow-0 md:gap-1  xl:gap-2"> */}
